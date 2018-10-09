6537dd2c7e0dd93a90c77d0209e472d4991b80ee
Siddhant Garg <sid7954@gmail.com>
2018-10-09 20:19:19 +0100

    Added async mode cluster start file

---
+++ Task-2 Async Node/run_code_template.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# cluster_utils.sh has helper function to start process on all VMs
+# it contains definition for start_cluster and terminate_cluster
+source cluster_utils.sh
+start_cluster lr_async.py cluster2
